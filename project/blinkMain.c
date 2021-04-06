ee68215ea4f04a5b38e58f843fdfd19cc913601d
acardoza4 <acardoza4@miners.utep.edu>
2021-04-06 17:48:19 -0600

    Minor change to blinkMain.c

--- project/blinkMain.c
+++ project/blinkMain.c
@@ -8,6 +8,7 @@ int main(void) {
   configureClocks(); /* setup master oscillator, CPU & peripheral clocks */
   switch_init();
   led_init();
+  reset();
   enableWDTInterrupts();	/* enable periodic interrupt */
 
   or_sr(0x18);		/* CPU off, GIE on */
